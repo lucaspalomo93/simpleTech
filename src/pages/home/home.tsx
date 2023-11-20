9b83e580573adf694e9a41fdc83f04878e4d58df
Lucas <lucaspalomo.1993@gmail.com>
2023-11-20 17:34:54 -0300

    added inspiration page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -12,23 +12,18 @@ function Home() {
       <CssBaseline />
       <div className='parent'>
         <div className='div1'>
-          {' '}
           <SideBar />{' '}
         </div>
         <div className='div2'>
-          {' '}
           <ColorPicker />{' '}
         </div>
         <div className='div3'>
-          {' '}
           <MainPictureDisplay />{' '}
         </div>
         <div className='div4'>
-          {' '}
           <ImageSidebar />{' '}
         </div>
         <div className='div5'>
-          {' '}
           <DescriptionFooter />{' '}
         </div>
       </div>
---
+++ src/pages/inspiration/inspiration.tsx
@@ -0,0 +1,5 @@
+function Inspiration() {
+  return <div>inspiration</div>;
+}
+
+export default Inspiration;
