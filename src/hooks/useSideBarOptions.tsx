a9b0a10b5ec931d512a391c50e50b2c7708792da
Lucas <lucaspalomo.1993@gmail.com>
2023-11-25 23:29:02 -0300

    fixed error that didnt allow to select a same subsection when clicking a section

--- src/hooks/useSideBarOptions.tsx
+++ src/hooks/useSideBarOptions.tsx
@@ -23,6 +23,7 @@ export const useSidebarOptions = () => {
       sidebarImagesArray: null,
       showInspirationProject: false,
     });
+    resetSubSectionValues();
     setSelectedSubSectionStyle('');
   }
 
