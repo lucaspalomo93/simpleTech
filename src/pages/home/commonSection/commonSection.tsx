c16bea7061ae85b4c54d7618aff6561ab89cd4c5
Lucas <lucaspalomo.1993@gmail.com>
2023-12-04 17:54:08 -0300

    removed default solution display in mobile

--- src/pages/home/commonSection/commonSection.tsx
+++ src/pages/home/commonSection/commonSection.tsx
@@ -4,20 +4,26 @@ import DescriptionFooter from '../../../components/descriptionFooter/description
 import ImageSidebar from '../../../components/imageSidebar/imageSidebar';
 import MainPictureDisplay from '../../../components/mainPictureDisplay/mainPictureDisplay';
 import { MouseEvent } from 'react';
+import { useMediaQuery, useTheme } from '@mui/material';
 
 interface PresentationButtonsProps {
   handleToggleSidebar: (
     event: MouseEvent<HTMLDivElement | HTMLButtonElement>
   ) => void;
   handleInspiracionClick: () => void;
+  showSidebar: boolean;
 }
 
 const CommonSection: React.FC<PresentationButtonsProps> = ({
   handleToggleSidebar,
   handleInspiracionClick,
+  showSidebar,
 }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
   return (
-    <div className='content'>
+    <div className={`content ${isMobile && showSidebar ? 'no-display' : ''}`}>
       <div className='main-content'>
         <div className='visualizer'>
           <div className='picker'>
